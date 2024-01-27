976f9c617831f5a0b5e6f69186fc8e7550fde205
Batuhan Tomo <batuhan.tomo@gmail.com>
2024-01-28 02:24:04 +0300

    Add App.vue component with necessary imports and template

---
+++ case/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Topbar from "./components/Topbar/Topbar.vue";
+import Banner from "./components/Banner/Banner.vue";
+import Feature from "./components/Feature/Feature.vue";
+import ProductShowcase from "./components/ProductShowcase/ProductShowcase.vue";
+import JoinUsSection from "./components/JoinUsSection/JoinUsSection.vue";
+import CarouselSection from "./components/CarouselSection/CarouselSection.vue";
+import FeaturedCollection from "./components/FeaturedCollection/FeaturedCollection.vue";
+import GlobalImpact from "./components/GlobalImpact/GlobalImpact.vue";
+import Footer from "./components/Footer/Footer.vue";
+</script>
+
+
+<template>
+  <div class="bg-[#fefbea]">
+    <Banner />
+    <ProductShowcase />
+    <JoinUsSection />
+    <CarouselSection />
+    <FeaturedCollection />
+    <GlobalImpact />
+    <Footer />
+  </div>
+</template>
+
+<style scoped></style>
